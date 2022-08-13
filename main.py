3b0204a30f1beb58c1f2e1667e91d342d527f8f3
VHSR3KorD3R <vhs_rekorder@msn.com>
2022-08-13 20:46:51 +0200

    get all book info in a dict

--- main.py
+++ main.py
@@ -19,10 +19,32 @@ def get_categories(soup):
     return list_categories
 
 def get_book_info(url):
+    book = {}
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     description = soup.select_one("article > p").text
-    print(description)
+    tds = soup.find('table', class_='table table-striped').findAll("td")
+    upc = tds[0].text
+    price_including_tax = float(tds[3].text.replace('Â£', ''))
+    price_excluding_tax = float(tds[2].text.replace('Â£', ''))
+    number_available = int(tds[5].text.replace('In stock (', '').replace(' available)', ''))
+    review_rating = soup.find('p', class_='star-rating')
+    review_rating = review_rating.get('class')[1]
+    image_url = 'http://books.toscrape.com/' + soup.find('div',class_='item active').find('img')['src'].replace('../../', '')
+    category = soup.find('ul', class_='breadcrumb').findNext('li').findNext('li').findNext('li').text
+    print(category)
+    book = dict([
+        ('product_page_url', url),
+        ('universal_ product_code', upc),
+        ('price_including_tax', price_including_tax),
+        ('price_excluding_tax', price_excluding_tax),
+        ('number_available', number_available),
+        ('product_description', description),
+        ('category', category),
+        ('review_rating', review_rating),
+        ('image_url', image_url),
+    ])
+    return book
 
 def get_books_from_category(url):
     response = requests.get(url)
