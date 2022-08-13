6029592e519c5c8e7c6404d85bd967da15301069
VHSR3KorD3R <vhs_rekorder@msn.com>
2022-08-13 05:31:19 +0200

    get book info(only description), get books from category

--- main.py
+++ main.py
@@ -7,15 +7,29 @@ def get_books_url_from_page(titles):
     books = {}
     for title in titles:
         temp = title.find('h3').find('a')['title']
-        book_url = url + title.find('h3').find('a')['href']
+        book_url = url +'catalogue/'+ title.find('h3').find('a')['href'].replace('../../../', '')
         books.update({temp: book_url})
     return books
 
 def get_categories(soup):
-    categories = soup.find(class_="side_categories")
+    list_categories = []
+    categories = soup.find('div', class_="side_categories").find('ul').find_all('li')[1:]
     for category in categories:
-        temp = category.find('a')
-        print(type(temp))
+        list_categories.append(url + category.find("a")["href"])
+    return list_categories
+
+def get_book_info(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    description = soup.select_one("article > p").text
+    print(description)
+
+def get_books_from_category(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    titles = soup.find_all(class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
+    books = get_books_url_from_page(titles)
+    return books
 
 if __name__ == '__main__':
     response = requests.get(url)
@@ -23,4 +37,9 @@ if __name__ == '__main__':
         soup = BeautifulSoup(response.text, 'html.parser')
         titles = soup.find_all(class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
         books_from_page = get_books_url_from_page(titles)
-        get_categories(soup)
+        all_categories_url = get_categories(soup)
+        category_url = all_categories_url[1]
+        books = get_books_from_category(category_url)
+        book_url = list(books.items())[1][1]
+        print(book_url)
+        get_book_info(book_url)
