76460da56aad2b1e06a16734fd431fce2cde6b5e
VHSR3KorD3R <vhs_rekorder@msn.com>
2022-08-20 02:35:38 +0200

    get categories return now a dict instead of a list, changed get_books_from_each_category and get_books_from_category created

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 url = 'http://books.toscrape.com/'
+
 
 def get_books_url_from_page(titles):
     books = {}
@@ -11,12 +13,17 @@ def get_books_url_from_page(titles):
         books.update({temp: book_url})
     return books
 
+
 def get_categories(soup):
-    list_categories = []
+    '''récupère la liste des catégories'''
+    list_categories = {}
     categories = soup.find('div', class_="side_categories").find('ul').find_all('li')[1:]
     for category in categories:
-        list_categories.append(url + category.find("a")["href"])
+        category_name = category.find("a").text.replace(' ', '').replace('\n', '')
+
+        list_categories.update({category_name: url + category.find("a")["href"]})
     return list_categories
+
 
 def get_book_info(url):
     book = {}
@@ -45,17 +52,40 @@ def get_book_info(url):
     ])
     return book
 
-def get_books_from_category(url):
+
+def get_books_url_from_category(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     titles = soup.find_all(class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
-    books = get_books_url_from_page(titles)
+    books_url = get_books_url_from_page(titles)
     next_page = soup.find(class_='next')
-    print(url)
+
     if next_page is not None:
-        print('ok')
-        next_page_url = url.replace('index.html', next_page.find('a')['href'])
-        books.update(get_books_from_category(next_page_url))
+        index = url.rindex("/")
+        partial_url = url[0:index + 1]
+        next_page_url = partial_url + next_page.find('a')['href']
+        print(next_page_url)
+        books_url.update(get_books_url_from_category(next_page_url))
+    return books_url
+
+def get_books_from_each_category(soup):
+    categories = get_categories(soup)
+    books = {}
+    for category_name, category_url in categories.items():
+        print(category_name)
+        books.update({category_name: get_books_from_category(category_url)})
+        print(books)
+    return books
+
+def get_books_from_category(category):
+    books_url = get_books_url_from_category(category)
+    books = []
+    for book_url in books_url.values():
+        books.append(get_book_info(book_url))
+    return books
+
+def save_in_csv(books):
+    
     return books
 
 if __name__ == '__main__':
@@ -64,10 +94,11 @@ if __name__ == '__main__':
         soup = BeautifulSoup(response.text, 'html.parser')
         titles = soup.find_all(class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
         books_from_page = get_books_url_from_page(titles)
-        all_categories_url = get_categories(soup)
+        '''all_categories_url = get_categories(soup)
         category_url = all_categories_url[1]
         books = get_books_from_category(category_url)
         print(len(books))
         for url in books.values():
-            print(get_book_info(url))
+            print(get_book_info(url))'''
+        get_books_from_each_category(soup)
 
